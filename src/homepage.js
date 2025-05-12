aba614e27d397290248a5c86bc02f84a1e6c68c6
Ghanisht Pareek <79847165+ghanishtpareek@users.noreply.github.com>
2025-05-12 15:36:47 +0000

    add basic styling to page

--- src/homepage.js
+++ src/homepage.js
@@ -1,11 +1,17 @@
 import foodImg1 from "./image1.jpg"
+import foodImg2 from "./image2.jpg"
+import foodImg3 from "./image3.jpg"
 
 function intro_section(content){
     const main_info = document.createElement("div")
     main_info.classList.add("front-images")
     const img1 = document.createElement("img")
     img1.src = foodImg1
-    main_info.append(img1)
+    const img2 = document.createElement("img")
+    img2.src = foodImg2
+    const img3 = document.createElement("img")
+    img3.src = foodImg3
+    main_info.append(img1, img2, img3)
     content.append(main_info)
 }
 
