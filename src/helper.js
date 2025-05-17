5df48cfb3ca4bc143a5e931e459354893cfd385e
Ghanisht Pareek <79847165+ghanishtpareek@users.noreply.github.com>
2025-05-17 07:34:15 +0000

    styling changes and helper function to create classes

---
+++ src/helper.js
@@ -0,0 +1,31 @@
+// export function nodeCreator(element,classNames=[],parentNode = "body"){
+//     console.log(element, parentNode)
+//     if(arguments.length > 2){
+//         const variable = document.createElement(element)
+//         if (!classNames == []){
+//             variable.classList.add(...classNames)
+//         }
+//         parentNode.appendChild(variable)
+//         return variable
+//     }
+//     else if(arguments.length > 1){
+//         const variable = document.createElement(element)
+//         variable.classList.add(...classNames)
+//         return variable
+//     }
+//     else{
+//         const variable = document.createElement(element)
+//         return variable
+//     }
+// }
+
+export function nodeCreator(element,classNames=[],parentNode = null){
+    const variable = document.createElement(element)
+    if(classNames.length > 0){
+        variable.classList.add(...classNames)
+    }
+    if(parentNode instanceof Node){
+        parentNode.appendChild(variable)
+    }
+    return variable
+}
--- src/homepage.js
+++ src/homepage.js
@@ -1,24 +1,34 @@
 import './introStyle.css'
+import { nodeCreator } from './helper'
 import foodImg1 from "../assets/image1.jpg"
 import foodImg2 from "../assets/image2.jpg"
 import foodImg3 from "../assets/image3.jpg"
 
+const images = [
+        { src: foodImg1, alt: "Delicious dish 1" },
+        { src: foodImg2, alt: "Delicious dish 2" },
+        { src: foodImg3, alt: "Delicious dish 3" }
+    ];
+
+
 function intro_section(content){
     const main_info = document.createElement("div")
     main_info.classList.add("front-images")
-    const img1 = document.createElement("img")
-    img1.src = foodImg1
-    const img2 = document.createElement("img")
-    img2.src = foodImg2
-    const img3 = document.createElement("img")
-    img3.src = foodImg3
-    main_info.append(img1, img2, img3)
+    images.forEach(imgData => {
+        const img = document.createElement("img");
+        img.src = imgData.src;
+        img.alt = imgData.alt;
+        main_info.appendChild(img);
+    });
     content.append(main_info)
 }
 
+function info_section(content){
+    const info = nodeCreator("div",content)
+    console.log(info)
+}
 export default function initial_load(){
-    console.log("loading the page")
     const content = document.querySelector("#content")
-    console.log(content)
     intro_section(content)
+    info_section(content)
 }
